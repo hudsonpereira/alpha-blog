02835e04358790fe46faa182b82473d7a26fa0a1
Hudson Pereira <hudson.byte@gmail.com>
2018-01-10 00:20:42 -0200

    user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'pages#home'
 
   resources :articles
+
+  get 'signup', to: 'users#signup', as: 'signup'
+  post 'signup', to: 'users#create'
 end
